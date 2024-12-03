b3d01da438d04d68e5a73aa707797d4986e7cc76
JianHungYe <146095272+JianHungYe@users.noreply.github.com>
2024-12-03 14:27:13 -0500

    Add files via upload

---
+++ adventofcoded3.py
@@ -0,0 +1,38 @@
+
+import re
+def get_file_data(file_name):
+    f = open(file_name)
+    data = []
+    for line in f:
+        data.append(line.rstrip())
+    return data
+
+file_data = get_file_data("input.txt")
+
+
+def part1():
+    inlineMul = []
+    mul = []
+    inlineNum = []
+    num = []
+    sum = 0
+    for line in file_data:
+        inlineMul = re.findall("mul\([0-9]+,[0-9]+\)", line)
+        for i in inlineMul:
+            mul.append(i)
+    for i in mul:
+        inlineNum = re.findall("[0-9]+", i)
+        for j in inlineNum:
+            num.append(j)
+    i = 0
+    while (i < len(num)-1):
+        product = int(num[i]) * int(num[i+1])
+        i+=2
+        sum = sum + product
+    print(sum)
+
+def part2:
+    
+
+part1()
+
