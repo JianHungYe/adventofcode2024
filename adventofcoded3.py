39c635721229a906bc7e7eb52524f1150ec138f6
JianHungYe <146095272+JianHungYe@users.noreply.github.com>
2024-12-03 14:47:41 -0500

    Update adventofcoded3.py

--- adventofcoded3.py
+++ adventofcoded3.py
@@ -1,4 +1,3 @@
-
 import re
 def get_file_data(file_name):
     f = open(file_name)
@@ -7,8 +6,8 @@ def get_file_data(file_name):
         data.append(line.rstrip())
     return data
 
-file_data = get_file_data("input.txt")
 
+file_data = get_file_data("input.txt")
 
 def part1():
     inlineMul = []
@@ -25,14 +24,36 @@ def part1():
         for j in inlineNum:
             num.append(j)
     i = 0
-    while (i < len(num)-1):
-        product = int(num[i]) * int(num[i+1])
-        i+=2
+    while (i < len(num) - 1):
+        product = int(num[i]) * int(num[i + 1])
+        i += 2
         sum = sum + product
     print(sum)
 
-def part2:
-    
+
+def part2():
+    inlineMul = []
+    mul = []
+    inlineNum = []
+    num = []
+    sum = 0
+    for line in file_data:
+
+        inlineMul = re.findall("mul\([0-9]+,[0-9]+\)", re.sub(r"don't\(\)(.*?)do\(\)", "",line))
+        for i in inlineMul:
+            mul.append(i)
+    for i in mul:
+        inlineNum = re.findall("[0-9]+", i)
+        for j in inlineNum:
+            num.append(j)
+    i = 0
+    while (i < len(num) - 1):
+        product = int(num[i]) * int(num[i + 1])
+        i += 2
+        sum = sum + product
+    print(sum)
+
 
 part1()
+part2()
 
